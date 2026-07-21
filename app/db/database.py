ad2eaa2c64c2ca4685013767b8f7ec4830558488
Raul Milos <raul.milos12@gmail.com>
2026-07-21 20:33:33 +0200

    db connection adjustments

--- app/db/database.py
+++ app/db/database.py
@@ -5,10 +5,13 @@ from sqlalchemy import text
 from app.config import DATABASE_URL
 
 
-engine = create_async_engine(DATABASE_URL, echo=True)
+engine = create_async_engine(
+    DATABASE_URL,
+    echo=True,
+    connect_args={"statement_cache_size": 0},
+)
 AsyncSessionLocal = async_sessionmaker(
     bind=engine, 
     class_=AsyncSession, 
     expire_on_commit=False
 )
-
