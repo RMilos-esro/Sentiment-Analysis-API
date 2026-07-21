34dd160716d49fa06857aada99a09d24bd8ce9d3
Raul Milos <raul.milos12@gmail.com>
2026-07-21 12:05:23 +0200

    update config & gitignore

--- app/config.py
+++ app/config.py
@@ -4,6 +4,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+HF_TOKEN = os.getenv("HF_TOKEN")
 
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL no está configurada en el archivo .env")
+
+if not HF_TOKEN:
+    raise ValueError("HF_TOKEN no está configurado en el archivo .env")
